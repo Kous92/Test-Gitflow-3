e83dd4d47cef6cf34fddddd6d61811628a9e4a17
Kous92 <kous92@gmail.com>
2023-03-28 14:14:50 +0200

    Définition de l'abstraction du service photo réseau.

---
+++ TestGitflow3/TestGitflow3/PhotoService/PhotoService.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoService.swift
+//  TestGitflow3
+//
+//  Created by Koussaïla Ben Mamar on 28/03/2023.
+//
+
+import Foundation
+
+protocol PhotoService {
+    func searchPhotos(with query: String) async -> Result<PhotoResponse, Error>
+}
